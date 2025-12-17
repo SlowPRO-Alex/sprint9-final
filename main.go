ac767eeb6e24bfe6e8b2ae0c5951800e6198c2c0
Alexander Chupin <alexander.pro@211.ru>
2025-12-18 01:51:24 +0700

    first commit

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"sync"
+	"time"
 )
 
 const (
@@ -12,29 +15,78 @@ const (
 // generateRandomElements generates random elements.
 func generateRandomElements(size int) []int {
 	// ваш код здесь
+	if size < 1 {
+		return []int{}
+	}
+	s := make([]int, size)
+	for i := 0; i < size; i++ {
+		rand.Seed(time.Now().UnixNano())
+		s[i] = rand.Intn(SIZE)
+	}
+	return s
 }
 
 // maximum returns the maximum number of elements.
 func maximum(data []int) int {
 	// ваш код здесь
+	if len(data) == 0 {
+		return 0
+	}
+	max := data[0]
+	for _, v := range data {
+		if v > max {
+			max = v
+		}
+	}
+	return max
 }
 
 // maxChunks returns the maximum number of elements in a chunks.
 func maxChunks(data []int) int {
 	// ваш код здесь
+	var wg sync.WaitGroup
+	switch {
+	case len(data) == 0:
+		return 0
+	case len(data)%CHUNKS != 0:
+		{
+			j := float64(CHUNKS) - float64(len(data)%CHUNKS)/(1/float64(CHUNKS))
+			for j > 0 {
+				data = append(data, 0)
+				j--
+			}
+		}
+	}
+	result := make([]int, CHUNKS)
+	for i := 0; i < CHUNKS; i++ {
+		wg.Add(1)
+		go func(ch []int) {
+			// уменьшаем счётчик, когда горутина завершает работу
+			defer wg.Done()
+			result = append(result, maximum(data[len(data)/CHUNKS*i:len(data)/CHUNKS*i+len(data)/CHUNKS]))
+		}(data)
+	}
+	wg.Wait()
+	return maximum(result)
 }
 
 func main() {
-	fmt.Printf("Генерируем %d целых чисел", SIZE)
+
+	fmt.Printf("Генерируем %d целых чисел\n", SIZE)
 	// ваш код здесь
+	data := generateRandomElements(SIZE)
 
 	fmt.Println("Ищем максимальное значение в один поток")
 	// ваш код здесь
-
+	tStart := time.Now()
+	max := maximum(data)
+	elapsed := time.Since(tStart)
 	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
 
-	fmt.Printf("Ищем максимальное значение в %d потоков", CHUNKS)
+	fmt.Printf("Ищем максимальное значение в %d потоков\n", CHUNKS)
 	// ваш код здесь
-
+	tStart = time.Now()
+	max = maxChunks(data)
+	elapsed = time.Since(tStart)
 	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
 }
--- main_test.go
+++ main_test.go
@@ -1,3 +1,75 @@
 package main
 
-// Пишите тесты в этом файле
+import (
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	// randSource источник псевдо случайных чисел.
+	// Для повышения уникальности в качестве seed
+	// используется текущее время в unix формате (в виде числа)
+	randSource = rand.NewSource(time.Now().UnixNano())
+	// randRange использует randSource для генерации случайных чисел
+	randRange = rand.New(randSource)
+)
+
+func TestGenerateRandomElements(t *testing.T) {
+	var size int
+	s := generateRandomElements(size) // Отправляем 0
+	assert.Equal(t, s, []int{})       // Проверяем, вернулся ли пустой слайс
+
+	s = generateRandomElements(-1) // Отправляем отрицательное значение
+	assert.Equal(t, s, []int{})    // Проверяем, вернулся ли пустой слайс
+}
+
+func TestMaximum(t *testing.T) {
+	var s []int
+	max := maximum(s)       // Отправляем пустой слайс
+	assert.Equal(t, max, 0) // Проверяем, вернулся ли 0
+
+	s = append(s, randRange.Intn(100))
+	max = maximum(s)           // Отправляем слайс с 1 элементом
+	assert.Equal(t, max, s[0]) // Проверяем, вернулся ли этот элемент
+
+	s = append(s, randRange.Intn(100)*-1)
+	max = maximum(s)           // Отправляем слайс с отрицательным числом
+	assert.Equal(t, max, s[0]) // Проверяем, вернулось ли максимальное значение слайса
+
+	s[1] = randRange.Intn(100)
+	s[0] = s[1]
+	s = append(s, s[0])
+	max = maximum(s)           // Отправляем слайс с одинаковыми элементами
+	assert.Equal(t, max, s[0]) // Проверяем, вернулось ли максимальное значение слайса
+
+	s[0] = 100
+	max = maximum(s)           // Отправляем слайс с максимальным элементом в начале
+	assert.Equal(t, max, s[0]) // Проверяем, вернулось ли максимальное значение слайса
+
+	s[len(s)-1] = 101
+	max = maximum(s)                  // Отправляем слайс с максимальным элементом в конце
+	assert.Equal(t, max, s[len(s)-1]) // Проверяем вернулось, ли максимальное значение слайса
+
+	for i := 0; i < len(s); i++ {
+		s[i] = s[i] * -1
+	}
+	max = maximum(s)           // Отправляем слайс только с отрицательными числами
+	assert.Equal(t, max, s[1]) // Проверяем вернулось, ли максимальное значение слайса
+}
+
+func TestMaxChunks(t *testing.T) {
+	var s []int
+	max := maxChunks(s)     // Отправляем пустой слайс
+	assert.Equal(t, max, 0) // Проверяем, вернулся ли 0
+
+	s = make([]int, CHUNKS+1) // Создаем слайс с количеством элементов не делящихся без остатка на CHUNKS(8)
+	for i := 0; i < len(s); i++ {
+		s[i] = randRange.Intn(100) // Заполняем слайс
+	}
+	max = maxChunks(s)
+	assert.NotEmpty(t, max) // Проверяем, что функция отработала и вернула значение
+
+}
